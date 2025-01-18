a16d455f463e7d615bb0136c48e3464c1cac8f29
yamauz <d-yamauchi@hitotocolabo.co.jp>
2025-01-18 22:02:53 +0900

    wip

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -40,6 +40,16 @@ starship init fish | source
 atuin init fish | source
 zoxide init fish | source
 
+# yazi
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 # vscode 
 # https://code.visualstudio.com/docs/terminal/shell-integration#_manual-installation
 string match -q "$TERM_PROGRAM" "vscode"
